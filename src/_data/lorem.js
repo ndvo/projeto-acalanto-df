d902aee59be46316fd0043c57d4bc9c764da50c1
Nelson do Vale Oliveira <nelson@ocastudios.com>
2020-06-10 15:02:12 -0300

    add more options to lorem

--- src/_data/lorem.js
+++ src/_data/lorem.js
@@ -11,10 +11,16 @@ async function fetchLoremIpsum(paragraphs, size) {
 module.exports = async () => {
   const teaser = await fetchLoremIpsum(1, 'short')
     .then(res => res.text());
+  const longTeaser = await fetchLoremIpsum(1, 'long')
+    .then(res => res.text());
   const full = await fetchLoremIpsum(3, 'long')
+    .then(res => res.text());
+  const longFull = await fetchLoremIpsum(5, 'long')
     .then(res => res.text());
   return {
     teaser,
-    full
+    longTeaser,
+    full,
+    longFull
   };
 };
