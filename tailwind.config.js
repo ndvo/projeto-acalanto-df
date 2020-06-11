b2354b10185505f1942ced82bf228bb23979a505
Nelson do Vale Oliveira <nelson@ocastudios.com>
2020-06-11 17:22:13 -0300

    Add generic custom color names to TailWind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
 module.exports = {
-	theme: {
-		extend: {
-			colors: {
-				"ws-blue": {
-					100: "#E6F0FF",
-					200: "#BFDAFF",
-					300: "#99C3FF",
-					400: "#4D97FE",
-					500: "#006AFE",
-					600: "#005FE5",
-					700: "#004098",
-					800: "#003072",
-					900: "#00204C",
-				},
-			},
-		},
-	},
-	variants: {
-		borderWidth: ["responsive", "last", "hover", "focus"],
-	},
-	plugins: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#648eff",
+        secondary: "#ff3232",
+        dark: "#2d2d2d",
+        light: "#d2f2ff",
+        "ws-blue": {
+          100: "#E6F0FF",
+          200: "#BFDAFF",
+          300: "#99C3FF",
+          400: "#4D97FE",
+          500: "#006AFE",
+          600: "#005FE5",
+          700: "#004098",
+          800: "#003072",
+          900: "#00204C",
+        },
+      },
+    },
+  },
+  variants: {
+    borderWidth: ["responsive", "last", "hover", "focus"],
+  },
+  plugins: [],
 };
